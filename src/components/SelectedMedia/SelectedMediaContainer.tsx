cbc8dbbca072d86bd9255541979a8566dc863b2e
Scott Curtis <ScottCurtisDeveloper@gmail.com>
2023-06-21 00:07:19 +1200

    Selects random songs from spotify

--- src/components/SelectedMedia/SelectedMediaContainer.tsx
+++ src/components/SelectedMedia/SelectedMediaContainer.tsx
@@ -1,4 +1,7 @@
+import Button from "@mui/material/Button";
 import { SelectedMediaList } from "./SelectedMediaList";
+import React from "react";
+import { redirect } from "react-router-dom";
 
 export type SpotifyMedia =
   | SpotifyApi.TrackObjectFull
@@ -23,5 +26,49 @@ interface SelectedMediaContainerProps {
 export const SelectedMediaContainer: React.FC<SelectedMediaContainerProps> = ({
   selectedMedia,
 }) => {
-  return <SelectedMediaList selectedMedia={selectedMedia} />;
+  const token = localStorage.getItem("token") ?? "";
+
+  if (!token) {
+    console.error("No token found - Auth test must be failing");
+  }
+
+  const generatePlaylist = () => {
+    const url =
+      "https://jrfg22ir6f.execute-api.ap-southeast-2.amazonaws.com/api/getrandomplaylist";
+    const params = new URLSearchParams();
+    params.set("token", token);
+    params.set("artists", Object.keys(selectedMedia?.artist ?? "").join(","));
+    params.set("playlistSize", "30");
+    const requestUrl = `${url}?${params.toString()}`;
+
+    fetch(requestUrl, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // setSearchResults(data);
+        // TODO: get the random songs from the api.
+      })
+      .catch((err) => {
+        // If unauthorized, redirect to login
+        if (err.status === 401) {
+          redirect("/login");
+        } else {
+          console.error(err);
+        }
+      })
+      .finally(() => {
+        // setIsSearching(false);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <SelectedMediaList selectedMedia={selectedMedia} />
+      <Button variant="contained" color="primary" onClick={generatePlaylist}>
+        Generate Playlist
+      </Button>
+    </React.Fragment>
+  );
 };
